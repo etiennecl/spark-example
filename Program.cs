43d5a2ea1cb2a78255814fce334fed5010caecc4
Radu Dumitru <raduoctav31@protonmail.com>
2023-05-30 14:20:29 -0400

    feat: Init

--- Program.cs
+++ Program.cs
@@ -1,3 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using Microsoft.Spark;
+using Microsoft.Spark.Sql;
 
-Console.WriteLine("Hello, World!");
+//using Microsoft.Spark;
+
+namespace SparkExample
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var spark = SparkSession.Builder().GetOrCreate();
+            
+            var spark = SparkSession
+                .Builder()
+                .Master("local[*]")
+                .AppName("connectors")
+                .Config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
+                .Config("spark.sql.catalog.dlf", "org.apache.iceberg.spark.SparkCatalog")
+                .Config("spark.sql.catalog.dlf.catalog-impl", "org.apache.iceberg.rest.RESTCatalog")
+                .Config("spark.sql.catalog.dlf.uri", "http://rest:8181")
+                .Config("spark.sql.catalog.dlf.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
+                .Config("spark.sql.catalog.dlf.s3.endpoint", "http://minio:9000")
+                .Config("spark.sql.defaultCatalog", "demo")
+                .Config("spark.eventLog.enabled", "true")
+                .Config("spark.eventLog.dir", "/home/iceberg/spark-events")
+                .Config("spark.history.fs.logDirectory", "/home/iceberg/spark-events")
+                .GetOrCreate();
+            // spark.SparkContext.Reference.Jvm
+            spark.SparkContext.SetLogLevel("ERROR");
+            
+            // Mock data from https://mockaroo.com/
+            var df = spark.Read().Json("facilities.json");
+            df.Show();
+            df.Write().Parquet("nom_fichier00.parquet");
+            
+            // var spark = SparkSession.Builder().AppName("Connectors").Config(new SparkConf()).Master("spark://ca1df3e51fb2:7077").GetOrCreate();
+            // var df = spark.Read().Json("brunetPharmacies.json");
+            // df.Show();
+            //
+            // var t = "";
+            //var df = spark.CreateDataFrame();
+            //df.Show();
+            //df.Write().Parquet("nom_fichier.parquet");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Spark Example
+
+## Steps
+- Follow https://github.com/dotnet/spark/blob/main/docs/getting-started/macos-instructions.md
+<br>_OR_
+- https://learn.microsoft.com/en-us/dotnet/spark/tutorials/get-started?tabs=linux
+- Install Spark version 3.2.1<br>
+https://archive.apache.org/dist/spark/
